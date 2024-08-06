ffe8d9011e1f6b20bb5604ecae8984ad7c0b9a02
HasanOmar1 <r3domar1@gmail.com>
2024-08-06 19:22:36 +0300

    video #3 , 49:27

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,6 @@
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AppRouter from "@routes/AppRouter";
+import "@styles/global.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<div>hello</div>);
+ReactDOM.createRoot(document.getElementById("root")!).render(<AppRouter />);
